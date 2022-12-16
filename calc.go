a0e3689a8d74b9dd2af41360ee3e09f3fcf999cb
DYZB <d.shawnyeo@gmail.com>
2022-12-16 11:13:06 +0800

    Create calc.go

---
+++ calc.go
@@ -0,0 +1,34 @@
+package calc
+
+import "fmt"
+
+func main(){
+	fmt.Println("Main for delivery")
+}
+
+
+// Add two numbers and return the result.
+func Add(val1, val2 int) int {
+	res := val1 + val2
+	return res
+}
+
+// Subtract two numbers and return the result.
+func Subtract(val1, val2 int) int {
+	return val1 - val2
+}
+
+// Multiply two numbers and return the result.
+func Multiply(val1, val2 int) int {
+	return val1 * val2
+}
+
+// Divide two numbers and return the result.
+func Divide(val1, val2 int) float64 {
+	defer func() {
+		if r := recover(); r != nil {
+			return
+		}
+	}()
+	return float64(val1) / float64(val2)
+}
